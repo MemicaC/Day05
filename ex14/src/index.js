170219e7ec28b669fb21979965603b9e832fddda
MemicaC <80220870+MemicaC@users.noreply.github.com>
2021-03-12 12:50:35 +0100

    Initial commit

---
+++ ex14/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    // Only change code below this line
+     var myNoun = "dog";
+     var myVerb = "ran";
+     var myAdjective = "big";
+     var myAdverb = "quickly";
+     var worldBlanks = myNoun + " " + myVerb + " " + myAdjective + " " + myAdverb + ".";
+    // Only change code above this line
+    return worldBlanks;
+  }
+  console.log(main());
+  module.exports = main;
